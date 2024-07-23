e417478f6cbc24f05b8663ddd4117db96fb7445d
suhy <hisuhy065@gmail.com>
2024-07-23 11:12:51 +0900

    메세지 보내기 기능

--- 00_make_url.py
+++ 00_make_url.py
@@ -1,10 +1,24 @@
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
-URL = 'https://api.telegram.org/bot'
+URL = f'https://api.telegram.org/bot{TOKEN}'
 
-print(f'{URL}{TOKEN}/getMe')
+method = 'getUpdates'
+
+res = requests.get(f'{URL}/{method}')
+
+res_dict = res.json()
+
+user_input = res_dict['result'][-1]['message']['text']
+user_id = res_dict['result'][-1]['message']['from']['id']
+
+print(user_id, user_input)
+
+SEND_MSG_URL = f'{URL}/sendMessage?chat_id={user_id}&text={user_input}'
+
+requests.get(SEND_MSG_URL)
